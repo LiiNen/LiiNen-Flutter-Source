a82d650dfa95339d13d9bf7a0850b65a6ec91942
LiiNen <kjeonghoon065@gmail.com>
2021-07-12 01:58:59 +0900

    Add banner with existing img

--- lib/navView/navItemView2.dart
+++ lib/navView/navItemView2.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(NavItemView2());
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class NavItemView2 extends StatefulWidget {
   @override
@@ -11,13 +10,54 @@ class _NavItemView2 extends State<NavItemView2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: SizedBox(
-              height: 200,
-              width: 200,
-              child: Image.asset('asset/image/sample2.jpeg'),
-          ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        children: <Widget>[
+          BannerContainer(),
+        ],
       ),
     );
   }
 }
+
+class BannerContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 500,
+      child: Swiper(
+        autoplay: true,
+        control: SwiperControl(),
+        itemCount: _imgSrcList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return BannerItem(_imgSrcList[index]);
+        },
+      )
+    );
+  }
+}
+
+class BannerItem extends StatelessWidget {
+  BannerItem(this._imgSrc);
+  final String _imgSrc;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GestureDetector(
+        onTap: null,
+        child: Image.network(_imgSrc)
+      )
+    );
+  }
+}
+
+final List<String> _imgSrcList = [
+  'https://raw.githubusercontent.com/LiiNen/indilist_private/master/image/pageView1.png',
+  'https://raw.githubusercontent.com/LiiNen/indilist_private/master/image/pageView2.png',
+  'https://raw.githubusercontent.com/LiiNen/indilist_private/master/image/pageView3.png',
+  'https://raw.githubusercontent.com/LiiNen/indilist_private/master/image/pageView4.png',
+  'https://raw.githubusercontent.com/LiiNen/indilist_private/master/image/pageView5.png',
+  'https://raw.githubusercontent.com/LiiNen/indilist_private/master/image/pageView6.png'
+];
