7ef6762e0d80534b78f422a174ef3ed24db5ea3e
LiiNen <kjeonghoon065@gmail.com>
2021-07-11 13:54:07 +0900

    Add horizontal scrolling view

--- lib/navView/navItemView1.dart
+++ lib/navView/navItemView1.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(NavItemView1());
-
 class NavItemView1 extends StatefulWidget {
   @override
   State<NavItemView1> createState() => _NavItemView1();
 }
 
 class _NavItemView1 extends State<NavItemView1> {
-  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          HorizontalContainer(),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200,
+            child: Center(
+              child: Text('hellooooo')
+            )
+          )
+        ],
+      )
+    );
+  }
+}
+
+final List<String> horList = List<String>.generate(20, (index) => 'hello $index');
+
+class HorizontalContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 150,
+      // width: horList.length * 150,
+      child: new ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: horList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return HorizontalItem(horList[index]);
+        },
+      )
+    );
+  }
+}
+
+class HorizontalItem extends StatelessWidget {
+  HorizontalItem(this._text);
+  final String _text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: Colors.orange,
+      margin: EdgeInsets.all(25),
       child: Center(
-          child: Text('hel')
-      ),
+        child: Text(_text)
+      )
     );
   }
 }
