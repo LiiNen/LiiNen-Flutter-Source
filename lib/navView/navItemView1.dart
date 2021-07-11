13e0519f426e69faa94a11f963bd00d47b4d69f9
LiiNen <kjeonghoon065@gmail.com>
2021-07-11 16:37:10 +0900

    Add inf scrolling view with horizon,vertical

--- lib/navView/navItemView1.dart
+++ lib/navView/navItemView1.dart
@@ -9,35 +9,54 @@ class _NavItemView1 extends State<NavItemView1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
         children: <Widget>[
           HorizontalContainer(),
           Container(
             width: MediaQuery.of(context).size.width,
-            height: 200,
+            height: 100,
+            color: Colors.lightGreen,
             child: Center(
-              child: Text('hellooooo')
+              child: Text('middle')
             )
-          )
-        ],
+          ),
+        ] + vertItemsList
       )
     );
   }
 }
 
 final List<String> horList = List<String>.generate(20, (index) => 'hello $index');
+final List<Widget> vertItemsList = List<Widget>.generate(horList.length, (index) {
+  return Column(
+    children: [
+      Container(
+        height: 100,
+        color: Colors.lightBlue,
+        child: Center(
+          child: Text(horList[index])
+        )
+      ),
+      Container(
+        height: 10,
+        color: Colors.white,
+      )
+    ],
+  );
+});
 
 class HorizontalContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 150,
-      // width: horList.length * 150,
       child: new ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: horList.length,
         itemBuilder: (BuildContext context, int index) {
-          return HorizontalItem(horList[index]);
+          return HorizontalItem(horList[index], Colors.orange);
         },
       )
     );
@@ -45,14 +64,15 @@ class HorizontalContainer extends StatelessWidget {
 }
 
 class HorizontalItem extends StatelessWidget {
-  HorizontalItem(this._text);
+  HorizontalItem(this._text, this._color);
   final String _text;
+  final Color _color;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 100,
       height: 100,
-      color: Colors.orange,
+      color: _color,
       margin: EdgeInsets.all(25),
       child: Center(
         child: Text(_text)
