536c6b9a6dad4ea91d6aefce6162e4a3541958a9
LiiNen <kjeonghoon065@gmail.com>
2021-07-24 15:47:03 +0900

    Add search result page container format

---
+++ lib/navView/searchView/searchResult/resultItemObject.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ResultClubObject {
+  String? thumbnailUrl;
+  String clubTitle;
+  String? clubContext;
+  ResultClubObject({this.thumbnailUrl, required this.clubTitle, this.clubContext});
+}
+
+class ResultClubContainer extends StatefulWidget {
+  @override
+  State<ResultClubContainer> createState() => _ResultClubContainer();
+}
+class _ResultClubContainer extends State<ResultClubContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
+
+class ResultClubItem extends StatefulWidget {
+  @override
+  State<ResultClubItem> createState() => _ResultClubItem();
+}
+class _ResultClubItem extends State<ResultClubItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
