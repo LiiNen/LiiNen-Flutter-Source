f3bfa8f79601adfc1dc02bceee3e5084f2423f26
LiiNen <kjeonghoon065@gmail.com>
2021-08-17 16:26:31 +0900

    Fix scroll all listview -> only gridview

--- lib/navView/searchView/searchTextView.dart
+++ lib/navView/searchView/searchTextView.dart
@@ -22,7 +22,7 @@ class _SearchTextView extends State<SearchTextView> {
           child: Column(
             children: [
               Container(
-                height: 40 * responsiveScale,
+                height: 40,
                 margin: EdgeInsets.symmetric(vertical: 16 * responsiveScale),
                 child: DecoratedBox(
                   decoration: BoxDecoration(
@@ -34,7 +34,7 @@ class _SearchTextView extends State<SearchTextView> {
                     textInputAction: TextInputAction.search,
                     decoration: InputDecoration(
                       border: InputBorder.none,
-                      contentPadding: EdgeInsets.only(left: 14, bottom: (30 * responsiveScale - 15) / 2), // TODO : 왜 30인지 모르겠음
+                      contentPadding: EdgeInsets.only(left: 14, bottom: (40 * responsiveScale - 15) / 2), // TODO : 왜 30인지 모르겠음
                       hintText: '어떤 모임을 찾으시나요?',
                       hintStyle: textStyle(color: Color(0xff8a8a8a), weight: 400, size: 12.0),
                     ),
--- lib/navView/searchView/searchView.dart
+++ lib/navView/searchView/searchView.dart
@@ -15,60 +15,66 @@ class _SearchView extends State<SearchView> {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> _searchByText = [
-      Container(
-        height: 40 * responsiveScale,
-        padding: EdgeInsets.symmetric(horizontal: 21 * responsiveScale),
-        child: GestureDetector(
-          onTap: () => {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => SearchTextView()))
-          },
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(2)),
-              color: Color(0xfff5f5f5),
-            ),
-            child: Center(
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  return Container(
-                    margin: EdgeInsets.only(left: 14 * responsiveScale),
-                    width: constraints.maxWidth,
-                    child: Text('어떤 모임을 찾으시나요?', style: textStyle(color: Color(0xff8a8a8a), weight: 400, size: 12.0), textAlign: TextAlign.left,)
-                  );
-                },
-              )
+    Container _searchByText = Container(
+      height: 40,
+      width: MediaQuery.of(context).size.width - 42 * responsiveScale,
+      margin: EdgeInsets.symmetric(horizontal: 21 * responsiveScale),
+      child: GestureDetector(
+        onTap: () => {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => SearchTextView()))
+        },
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(2)),
+            color: Color(0xfff5f5f5),
+          ),
+          child: Center(
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return Container(
+                  margin: EdgeInsets.only(left: 14 * responsiveScale),
+                  width: constraints.maxWidth,
+                  child: Text('어떤 모임을 찾으시나요?', style: textStyle(color: Color(0xff8a8a8a), weight: 400, size: 12.0), textAlign: TextAlign.left,)
+                );
+              },
             )
           )
         )
       )
-    ];
+    );
 
-    List<Widget> _searchByCategory = [
-      TitleContainer('카테고리'),
-      SizedBox(height: 4),
-      Container(
-        child: categoryGridContainer(context)
-      )
-    ];
+    Container _searchByCategory = Container(
+      margin: EdgeInsets.symmetric(horizontal: 21 * responsiveScale),
+      child: categoryGridContainer(context)
+    );
 
     return Scaffold(
       appBar: MainViewAppBar(title: '탐색'),
       body: Container(
+        width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(color: Colors.white),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: GestureDetector(
-            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              children: <Widget>[SizedBox(height: 16 * responsiveScale)]
-                + _searchByText
-                + [SizedBox(height: 12 * responsiveScale)]
-                + _searchByCategory
-            )
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 16,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _searchByText,
+                    SizedBox(height: 12),
+                    TitleContainer('카테고리'),
+                  ],
+                )
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 108),
+                child: _searchByCategory,
+              )
+            ]
           )
         )
       )
@@ -89,11 +95,24 @@ class CategoryItemClass {
   CategoryItemClass({required this.title, required this.context, required this.imgSrc});
 }
 
-final List<CategoryItemClass> categoryList = [
+List<CategoryItemClass> categoryList = [
   CategoryItemClass(title: '운동', context: '(스포츠/피트니스)', imgSrc: 'asset/category/sport.jpeg'),
   CategoryItemClass(title: '공예', context: '(DIY)', imgSrc: 'asset/category/diy.jpeg'),
   CategoryItemClass(title: '예술', context: '(악기/그림)', imgSrc: 'asset/category/art.jpeg'),
   CategoryItemClass(title: '자기계발', context: '(독서/스터디)', imgSrc: 'asset/category/reading.jpeg'),
+  CategoryItemClass(title: 'IT', context: '(개발/디자인)', imgSrc: 'asset/category/it.jpeg'),
+  CategoryItemClass(title: '오락', context: '(온라인/보드게임)', imgSrc: 'asset/category/boardGame.jpeg'),
+  CategoryItemClass(title: '자유주제', context: '(기타)', imgSrc: 'asset/category/etc.jpeg'),
+  CategoryItemClass(title: 'IT', context: '(개발/디자인)', imgSrc: 'asset/category/it.jpeg'),
+  CategoryItemClass(title: '오락', context: '(온라인/보드게임)', imgSrc: 'asset/category/boardGame.jpeg'),
+  CategoryItemClass(title: '자유주제', context: '(기타)', imgSrc: 'asset/category/etc.jpeg'),
+  CategoryItemClass(title: '오락', context: '(온라인/보드게임)', imgSrc: 'asset/category/boardGame.jpeg'),
+  CategoryItemClass(title: '자유주제', context: '(기타)', imgSrc: 'asset/category/etc.jpeg'),
+  CategoryItemClass(title: 'IT', context: '(개발/디자인)', imgSrc: 'asset/category/it.jpeg'),
+  CategoryItemClass(title: '오락', context: '(온라인/보드게임)', imgSrc: 'asset/category/boardGame.jpeg'),
+  CategoryItemClass(title: '자유주제', context: '(기타)', imgSrc: 'asset/category/etc.jpeg'),
+  CategoryItemClass(title: '오락', context: '(온라인/보드게임)', imgSrc: 'asset/category/boardGame.jpeg'),
+  CategoryItemClass(title: '자유주제', context: '(기타)', imgSrc: 'asset/category/etc.jpeg'),
   CategoryItemClass(title: 'IT', context: '(개발/디자인)', imgSrc: 'asset/category/it.jpeg'),
   CategoryItemClass(title: '오락', context: '(온라인/보드게임)', imgSrc: 'asset/category/boardGame.jpeg'),
   CategoryItemClass(title: '자유주제', context: '(기타)', imgSrc: 'asset/category/etc.jpeg')
@@ -101,11 +120,13 @@ final List<CategoryItemClass> categoryList = [
 
 Widget categoryGridContainer(BuildContext context) {
   return Container(
-    margin: EdgeInsets.symmetric(horizontal: 21 * responsiveScale),
     child: GridView.count(
+      padding: EdgeInsets.symmetric(vertical: 15 * responsiveScale),
+      scrollDirection: Axis.vertical,
+      physics: ScrollPhysics(),
       shrinkWrap: true,
       crossAxisCount: 2, crossAxisSpacing: 15 * responsiveScale,
-      mainAxisSpacing: 12 * responsiveScale,
+      mainAxisSpacing: 15 * responsiveScale,
       childAspectRatio: 1.7,
       children: List.generate(categoryList.length, (index){
         return Container(
